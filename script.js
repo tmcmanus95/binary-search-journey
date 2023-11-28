94796508f99cf0327cfd6705462314b7e372db2e
tmcmanus95 <tmcmanus95@gmail.com>
2023-11-28 15:17:25 -0600

    edited styling

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
 const guessContainer = document.querySelector("#guesses-container");
-const sortedArray = Array.from({ length: 1000000 }, (_, index) => index + 1);
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-const target = getRandomInt(1000000);
-let targetLine = document.createElement("h1");
-targetLine.textContent = `The target is ${target}`;
-guessContainer.appendChild(targetLine);
-function binarySearch(array, target) {
+const newRandomNumberButton = document.querySelector(
+  "#new-random-number-button"
+);
+
+function binarySearch() {
+  guessContainer.textContent = "";
+  const array = Array.from({ length: 1000000 }, (_, index) => index + 1);
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const target = getRandomInt(1000000);
+
   let bottom = 0;
   let top = array.length - 1;
   let guessNumber = 0;
   let rangeLine = document.createElement("h2");
   rangeLine.textContent = `The range is ${bottom} - ${top}`;
+  let targetLine = document.createElement("h1");
+  targetLine.textContent = `The target is ${target}`;
+
+  guessContainer.appendChild(targetLine);
+
   guessContainer.appendChild(rangeLine);
   while (bottom <= top) {
     let middle = Math.floor((top + bottom) / 2);
     let guess = array[middle];
     if (guess === target) {
+      let hoorayLine = document.createElement("h1");
+      hoorayLine.textContent = `${target} found`;
+
+      hoorayLine.style.backgroundColor = "darkseagreen";
+      hoorayLine.classList.add("fade-in");
+      guessContainer.appendChild(hoorayLine);
+      setTimeout(() => {
+        hoorayLine.style.opacity = 1;
+      }, 100 * guessNumber);
+
       return middle;
     } else if (guess < target) {
       guessNumber++;
       const tooLowLine = document.createElement("h2");
       tooLowLine.textContent = `Guess ${guessNumber}: ${guess} was too low`;
       tooLowLine.style.backgroundColor = "lightblue";
+      tooLowLine.classList.add("fade-in");
       guessContainer.appendChild(tooLowLine);
+      setTimeout(() => {
+        tooLowLine.style.opacity = 1;
+      }, 100 * guessNumber);
+
       bottom = middle + 1;
     } else {
       guessNumber++;
@@ -32,21 +55,16 @@ function binarySearch(array, target) {
       const tooHighLine = document.createElement("h2");
       tooHighLine.textContent = `Guess ${guessNumber}: ${guess} was too high`;
       tooHighLine.style.backgroundColor = "indianred";
-
+      tooHighLine.classList.add("fade-in");
       guessContainer.appendChild(tooHighLine);
+      setTimeout(() => {
+        tooHighLine.style.opacity = 1;
+      }, 100 * guessNumber);
+
       top = middle - 1;
     }
   }
   return -1;
 }
 
-const result = binarySearch(sortedArray, target);
-
-if (result !== -1) {
-  console.log(`${target} found at index ${result}`);
-  let hoorayLine = document.createElement("h1");
-  hoorayLine.textContent = `${target} found`;
-  guessContainer.appendChild(hoorayLine);
-} else {
-  console.log(`${target} not found in the array`);
-}
+newRandomNumberButton.addEventListener("click", binarySearch);

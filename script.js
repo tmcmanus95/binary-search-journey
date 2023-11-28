422ce2fb0130f29a38e68c9148af3d7f797a03e8
tmcmanus95 <tmcmanus95@gmail.com>
2023-11-28 15:37:00 -0600

    edited stylings

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function binarySearch() {
   const target = getRandomInt(1000000);
 
   let bottom = 0;
-  let top = array.length - 1;
+  let top = array.length;
   let guessNumber = 0;
   let rangeLine = document.createElement("h2");
   rangeLine.textContent = `The range is ${bottom} - ${top}`;
@@ -28,7 +28,6 @@ function binarySearch() {
     if (guess === target) {
       let hoorayLine = document.createElement("h1");
       hoorayLine.textContent = `${target} found`;
-
       hoorayLine.style.backgroundColor = "darkseagreen";
       hoorayLine.classList.add("fade-in");
       guessContainer.appendChild(hoorayLine);
@@ -40,15 +39,22 @@ function binarySearch() {
     } else if (guess < target) {
       guessNumber++;
       const tooLowLine = document.createElement("h2");
-      tooLowLine.textContent = `Guess ${guessNumber}: ${guess} was too low`;
       tooLowLine.style.backgroundColor = "lightblue";
       tooLowLine.classList.add("fade-in");
+      tooLowLine.textContent = `Guess ${guessNumber}: ${guess} was too low`;
+      const newRangeLine = document.createElement("h4");
+      newRangeLine.classList.add("fade-in");
       guessContainer.appendChild(tooLowLine);
+      guessContainer.appendChild(newRangeLine);
       setTimeout(() => {
         tooLowLine.style.opacity = 1;
       }, 100 * guessNumber);
+      setTimeout(() => {
+        newRangeLine.style.opacity = 1;
+      }, 100 * guessNumber);
 
       bottom = middle + 1;
+      newRangeLine.textContent = `\n\nNew Range: ${bottom} - ${top}`;
     } else {
       guessNumber++;
 
@@ -56,12 +62,20 @@ function binarySearch() {
       tooHighLine.textContent = `Guess ${guessNumber}: ${guess} was too high`;
       tooHighLine.style.backgroundColor = "indianred";
       tooHighLine.classList.add("fade-in");
+      const newRangeLine = document.createElement("h4");
+      newRangeLine.classList.add("fade-in");
+
       guessContainer.appendChild(tooHighLine);
+      guessContainer.appendChild(newRangeLine);
       setTimeout(() => {
         tooHighLine.style.opacity = 1;
       }, 100 * guessNumber);
+      setTimeout(() => {
+        newRangeLine.style.opacity = 1;
+      }, 100 * guessNumber);
 
       top = middle - 1;
+      newRangeLine.textContent = `\n\nNew Range: ${bottom} - ${top}`;
     }
   }
   return -1;

1b30b715b6791318448bd7202a3b2ebfb4330c26
tmcmanus95 <tmcmanus95@gmail.com>
2023-11-28 18:49:20 -0600

    edited mobile

--- script.js
+++ script.js
@@ -15,9 +15,16 @@ function binarySearch() {
   let top = array.length - 1;
   let guessNumber = 0;
   let rangeLine = document.createElement("h2");
-  rangeLine.textContent = `Starting range is: ${bottom} - ${top}`;
+  rangeLine.textContent = `Starting range: ${bottom} - ${top}`;
+  rangeLine.style.backgroundColor = "lemonchiffon";
   let targetLine = document.createElement("h1");
-  targetLine.textContent = `The target is ${target}`;
+  let targetSpan = document.createElement("span");
+  targetSpan.style.backgroundColor = "darkseagreen";
+  targetLine.textContent = `The target is: `;
+  targetSpan.textContent = `${target}`;
+  targetSpan.style.padding = "5px";
+  targetSpan.style.borderRadius = "5px";
+  targetLine.appendChild(targetSpan);
 
   guessContainer.appendChild(targetLine);
 
@@ -28,8 +35,10 @@ function binarySearch() {
     let guess = array[middle];
     if (guess === target) {
       let hoorayLine = document.createElement("h1");
-      hoorayLine.textContent = `${target} found`;
+      hoorayLine.textContent = `Guess ${guessNumber + 1}: ${target} found`;
       hoorayLine.style.backgroundColor = "darkseagreen";
+      hoorayLine.style.padding = "5px";
+      hoorayLine.style.borderRadius = "5px";
       hoorayLine.classList.add("fade-in");
       guessContainer.appendChild(hoorayLine);
       setTimeout(() => {
@@ -63,7 +72,7 @@ function binarySearch() {
       const tooHighLine = document.createElement("h2");
       console.log(`guess ${guessNumber}'s middle is ${middle}`);
       tooHighLine.textContent = `Guess ${guessNumber}: ${middle} - Too high`;
-      tooHighLine.style.backgroundColor = "indianred";
+      tooHighLine.style.backgroundColor = "salmon";
       tooHighLine.classList.add("fade-in");
       const newRangeLine = document.createElement("h4");
       newRangeLine.classList.add("fade-in");
